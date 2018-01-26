8232eb0bf67b9d35080d4cc370d4f5f04213ffd2
dante <daniellewinski95@gmail.com>
2018-01-26 21:37:35 +0100

    Unit Controller.

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class UnitController : MonoBehaviour
+	{
+		[SerializeField]
+		private float initialLifetime = 20;
+
+		private float lifetime;
+		void Start()
+		{
+			lifetime = initialLifetime;
+		}
+
+		void Update()
+		{
+			lifetime -= Time.deltaTime;
+			if (lifetime <= 0)
+				Debug.Log("Bang");
+			else
+				Debug.Log(""+ (int)lifetime);
+		}
+	}
+}
