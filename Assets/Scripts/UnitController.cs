c13ddb2837add499ca95d895a2c03aa570fc51c4
dante <daniellewinski95@gmail.com>
2018-01-26 22:04:49 +0100

    UnitController input

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -6,17 +6,23 @@ namespace Assets.Scripts
 {
 	public class UnitController : MonoBehaviour
 	{
-		[SerializeField]
-		private float initialLifetime = 20;
+		[SerializeField] private float initialLifetime = 20;
 
+		[SerializeField] private float speed = 10.0f;
+		
 		private float lifetime;
+		private Rigidbody2D body;
+		
 		void Start()
 		{
 			lifetime = initialLifetime;
+			body = GetComponent<Rigidbody2D>();
 		}
 
 		void Update()
 		{
+			body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+			
 			lifetime -= Time.deltaTime;
 			if (lifetime <= 0)
 				Debug.Log("Bang");
