c5e2ff203a972519d2c3e903a86bc124f86a1261
dante <daniellewinski95@gmail.com>
2018-01-26 22:31:26 +0100

    Projectiles.

---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class ProjectileController : MonoBehaviour
+	{
+		[SerializeField] private float speed = 10.0f;
+
+		private Rigidbody2D body;
+
+		private Vector2 velocity;
+		
+		public Vector2 Velocity
+		{
+			set { velocity = value * speed; }
+		}
+		
+		void Start()
+		{
+			body = GetComponent<Rigidbody2D>();
+		}
+
+		void Update()
+		{
+			body.velocity = velocity * Time.deltaTime;
+		}
+	}
+}
--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -9,6 +9,8 @@ namespace Assets.Scripts
 		[SerializeField] private float initialLifetime = 20;
 
 		[SerializeField] private float speed = 10.0f;
+
+		[SerializeField] private GameObject projectilePrefab = null;
 		
 		private float lifetime;
 		private Rigidbody2D body;
@@ -21,13 +23,22 @@ namespace Assets.Scripts
 
 		void Update()
 		{
+			
+			
 			body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+
+			if (Input.GetButtonUp("Jump"))
+			{
+				ProjectileController projectile = (Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject).GetComponent<ProjectileController>();
+				projectile.Velocity = body.velocity.normalized;
+			}
 			
 			lifetime -= Time.deltaTime;
 			if (lifetime <= 0)
 				Debug.Log("Bang");
 			else
 				Debug.Log(""+ (int)lifetime);
+			
 		}
 	}
 }
