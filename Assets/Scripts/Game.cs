c23cacdb6eccd9573fe8c8fbd216a78e62c32b3b
dante <daniellewinski95@gmail.com>
2018-01-28 07:57:55 +0100

    Game over handling.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -21,6 +21,13 @@ namespace Assets.Scripts
 
         private AudioSource audioSource;
 
+		private bool gameOver = false;
+
+		public bool GameOver
+		{
+			get { return gameOver; }
+		}
+
         void Start()
 		{
 			players = FindObjectsOfType<Player>().OfType<Player>().ToList();
@@ -40,7 +47,7 @@ namespace Assets.Scripts
 			deadline -= Time.deltaTime;
 			timer.text = "" + (int)Mathf.Ceil(Mathf.Clamp(deadline, 0.0f, 10000.0f));
 
-			if (isGameOver() != -2)
+			if (isGameOver() != -2 && !gameOver)
 			{
 				Debug.Log("GameOver");
                 
@@ -50,7 +57,11 @@ namespace Assets.Scripts
                     audioSource.Play();
                 }
 
-            }
+				GameObject survivor = FindObjectOfType<UnitController>().gameObject;
+				Camera.main.gameObject.transform.position = survivor.transform.position + survivor.transform.forward*5 + survivor.transform.up * 5;
+				Camera.main.transform.LookAt(survivor.transform);
+				gameOver = true;
+			}
 
         }
 
